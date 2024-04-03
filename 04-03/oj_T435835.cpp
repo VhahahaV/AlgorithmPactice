5b4c1c54308b055cc91b0432cf5ba20df0bf3614
VhahahaV <czq666@sjtu.edu.cn>
2024-04-03 10:46:58 +0800

    04-03 1'

---
+++ 04-03/oj_T435835.cpp
@@ -0,0 +1,4 @@
+//
+// Created by VhahahaV on 2024/4/3.
+//
+#include <bits/stdc++.h>
---
+++ 04-03/oj_T436703.cpp
@@ -0,0 +1,39 @@
+//
+// Created by VhahahaV on 2024/4/3.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> arr;
+vector<int> ans;
+void get_days(int start,int end){
+    if (end < 0)
+        return;
+    if(arr[start] >= arr[end]){
+        ans[start] += ans[end];
+        int newEnd = end - ans[end] ;
+        get_days(start,newEnd);
+    }
+    else
+        return;
+
+}
+int main(){
+    int N;
+    cin >> N;
+
+    arr.reserve(N);
+    ans.reserve(N);
+    for (int i = 0; i < N; ++i) {
+        int tmp;
+        cin>>tmp;
+        arr.emplace_back(tmp);
+    }
+    ans.insert(ans.begin(),N,1);
+    for(int i = 1 ; i < N ; i++){
+        get_days(i,i-1);
+    }
+    for(auto i : ans)
+        cout << i << " ";
+
+}
