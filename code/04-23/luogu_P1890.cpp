2386f0fc5760e52db40f2fcec7bf38cc8e2492cb
VhahahaV <czq666@sjtu.edu.cn>
2024-04-23 16:36:05 +0800

    04-23 5'

--- code/04-23/luogu_P1890.cpp
+++ code/04-23/luogu_P1890.cpp
@@ -2,34 +2,76 @@
 // Created by VhahahaV on 2024/4/23.
 //
 
-//10min -> 90 points
-//需要用线段树优化，来日再战
-//线段树参考： https://zhuanlan.zhihu.com/p/106118909
+////10min -> 90 points
+////需要用线段树优化，来日再战
+////线段树参考： https://zhuanlan.zhihu.com/p/106118909
 
+////写完线段树模板的思考：这题不需要区间修改，，能用线段树的原因是gcd的运算满足交换了，从而可以用区间来存
+////线段树第一版 差0.05s,继续优化
+////
 #include "bits/stdc++.h"
 
+
 using namespace std;
+int n,m;
+//    默认a>b
+int gcd(int a , int b ){
+    if (a < b)
+        swap(a,b);
+    int tmp = a % b;
+    while (tmp){
+        a = b;
+        b = tmp;
+        tmp = a % b;
+    }
+    return b;
+};
+////线段树方案，将区间存在线段树中，直接查询,数据不是很大，直接用int搓
+vector<int> tree;
+//vector<int> mark;
+//由于没有修改，所以可以不用懒标记mark
+vector<int> nums;
+
+void build(int left = 1,int right = n,int p = 1) {
+    if (left == right){
+        tree[p] = nums[left];
+    }
+    else {
+        int mid = (left + right) / 2;
+        build(left,mid,2*p);
+        build(mid+1,right,2*p+1);
+        tree[p] = gcd(tree[2*p+1],tree[2*p]);
+    }
+}
+
+int query(const int &tLeft,const int &tRight ,int p = 1, int cLeft = 1, int cRight = n){
+    if (cLeft > tRight || cRight < tLeft)
+        return 0;
+    else if(cLeft >= tLeft && cRight <= tRight){
+        return tree[p];
+    }
+    else{
+        int mid = (cLeft + cRight) / 2;
+        int child1 = query(tLeft,tRight,2*p,cLeft,mid);
+        int child2 = query(tLeft,tRight,2*p+1,mid+1,cRight);
+        if (!child2 || !child1)
+            return child1 ? child1 : child2;
+        return gcd(child1,child2);
+    }
+}
 
 int main(){
-    int n,m;
+////优化输入
+//取消同步流
+//同步流 将时间从 1.05s -> 284ms easy
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
     cin >> n >> m;
-    vector<int> nums(n+1);
+    nums.resize(n+1),tree.resize(n<<2);
     for (int i = 1; i <= n; ++i) {
         cin >> nums[i];
     }
-//    默认a>b
-    auto gcd = [&](int a , int b ) -> int {
-        if (a < b)
-            swap(a,b);
-        int tmp = a % b;
-        while (tmp){
-            a = b;
-            b = tmp;
-            tmp = a % b;
-        }
-        return b;
-    };
-
+//    顺序做gcd（辗转相除）的思想，每次操作都要遍历一次，复杂度高
     auto findMaxGcd = [&](int l ,int r)-> int {
         if (l == r)
             return nums[l];
@@ -41,10 +83,14 @@ int main(){
         }
         return res;
     };
+    build();
     while (m--){
         int l,r;
         cin >> l >> r;
-        cout << findMaxGcd(l,r) << '\n';
+        cout << query(l,r) << '\n';
+//        cout << findMaxGcd(l,r) << '\n';
     }
+
+
     return 0;
 }
--- code/04-23/luogu_P3372.cpp
+++ code/04-23/luogu_P3372.cpp
@@ -6,8 +6,90 @@
 ////O(log n)的区间修改，还可以同时支持多种操作（加、乘），更具通用性。
 
 using namespace std;
+//线段树模板题
+//90min
+using ll = long long;
+ll n,m;
+vector<ll> tree; //线段树是一颗平衡二叉树，直接用数组存
+vector<ll> mark; //懒标记数组,每次更新的时候，只过更新到第一次访问到的，目标更新区间的子区间的值，就不往下传了。
+vector<ll> nums; //储存数据用的数组
+//ATTENTION : 线段树的右节点区间长度 <= 左节点区间长度
+void build(ll left = 1,ll right = n,ll p = 1){
+    if (left == right){
+//        叶子节点，直接建树
+        tree[p] = nums[left];
+    }
+    else{
+        ll mid = (left+right)/2;
+        build(left,mid,2*p);
+        build(mid+1,right,2*p+1);
+        tree[p] = tree[2*p] + tree[2*p+1];
+    }
+}
+
+//懒标记向上传一层
+void push_down(ll p, ll len){
+    mark[2*p] += mark[p];
+    mark[2*p+1] += mark[p];
+//    更新tree数组需要乘上区间长度
+    tree[2*p] += mark[p] * (len - len/2);
+    tree[2*p+1] += mark[p] * (len/2);
+//  清除上一层的懒标记
+    mark[p] = 0;
+}
+//ll tLeft , ll tRight 为目标区间（要更新的区间） ll p = 1, ll cLeft = 1, ll cRight = n 为当前处于的区间，从该区间不断二分去拟合更新区间
+void update(ll tLeft , ll tRight ,ll val ,ll p = 1, ll cLeft = 1, ll cRight = n){
+    if (cRight < tLeft || cLeft > tRight)
+        return ;
+    else if(cLeft >= tLeft && cRight <= tRight){
+        mark[p] += val;
+        tree[p] += val*(cRight-cLeft+1);
+    }
+    else{
+//        如果当前区间与目标区间有交叉，则向下传递
+        ll mid = (cLeft + cRight) / 2;
+        push_down(p,cRight-cLeft+1);
+        update(tLeft,tRight,val,2*p,cLeft,mid);
+        update(tLeft,tRight,val,2*p+1,mid+1,cRight);
+        tree[p]  = tree[2*p] + tree[2*p+1];
+    }
+}
+
+ll query(ll tLeft , ll tRight ,ll p = 1, ll cLeft = 1, ll cRight = n){
+    if (cRight < tLeft || cLeft > tRight)
+        return 0;
+    else if(cLeft >= tLeft && cRight <= tRight){
+        return tree[p];
+    }
+    else{
+        ll mid = (cLeft + cRight) / 2;
+        push_down(p,cRight-cLeft+1);
+        return (query(tLeft,tRight,2*p,cLeft,mid) + query(tLeft,tRight,2*p+1,mid+1,cRight));
+    }
+}
+
 
 int main(){
-
+    cin >> n >> m;
+//    这里tree 和 mark 数组由于存的是一个树的结构，不能用n大小的空间，也不能用 n*n的空间，不然会RE，测试后4*n的空间足够。
+    tree.resize(n*4),mark.resize(n*4),nums.resize(n+1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> nums[i];
+    }
+    build();
+    while (m--){
+        int model;
+        cin >> model;
+        if (model == 1){
+            ll l,r,k;
+            cin >> l >> r >> k;
+            update(l,r,k);
+        }
+        else{
+            ll l,r;
+            cin >> l >> r;
+            cout << query(l,r) << '\n';
+        }
+    }
     return 0;
 }
